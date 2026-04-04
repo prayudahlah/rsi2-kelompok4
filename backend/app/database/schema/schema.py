847f23b0d28d64dbc0334161f97bc088d1813c5a
bdyhauliaa <auliarahmabidayah26@gmail.com>
2026-04-04 12:52:26 +0700

    fix : update registration schema

--- backend/app/database/schema/schema.py
+++ backend/app/database/schema/schema.py
@@ -1,14 +1,30 @@
 from sqlmodel import SQLModel, Field, Relationship
-from typing import Optional, List
+from datetime import datetime
+from typing import Optional
+from pydantic import EmailStr
+from sqlalchemy import func
 
 
-class Role(SQLModel, table=True):
-    __tablename__ = "role"
-
+class Account(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str = Field(max_length=255, unique=True, index=True)
+    user_id: int = Field(foreign_key="user.id")
+    role_id: int = Field(foreign_key="role.id")
+    email: EmailStr = Field(unique=True)
+    username: str = Field(max_length=16)
+    password: str
 
-    accounts: List["Account"] = Relationship(back_populates="role")
+    created_at: datetime = Field(
+        default_factory=datetime.now, sa_column_kwargs={"server_default": func.now()}
+    )
+    updated_at: datetime = Field(
+        default_factory=datetime.now,
+        sa_column_kwargs={"onupdate": func.now(), "server_default": func.now()},
+    )
+
+    user: "User" = Relationship(back_populates="accounts")
+    role: "Role" = Relationship(back_populates="accounts")
+
+    logs: list["Log"] = Relationship(back_populates="account")
 
 class Registration(SQLModel, table=True):
     __tablename__ = "registration"
