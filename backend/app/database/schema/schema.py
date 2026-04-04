86c7de6dc87adcad2f2679c701dd8cba1fd49e9d
Dien <akbardian64@gmail.com>
2026-04-04 13:29:05 +0700

    refactor: fix dir database

--- backend/app/database/schema/schema.py
+++ backend/app/database/schema/schema.py
@@ -1,21 +1,44 @@
-from sqlmodel import SQLModel, Field, Relationship
 from datetime import datetime
+from sqlmodel import SQLModel, Field, Relationship, func
 from typing import Optional
-from pydantic import EmailStr
-from sqlalchemy import func
 
 
-class Account(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    user_id: int = Field(foreign_key="user.id")
-    role_id: int = Field(foreign_key="role.id")
-    email: EmailStr = Field(unique=True)
-    username: str = Field(max_length=16)
-    password: str
-
+class Event(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True, nullable=False)
+    name: str = Field(max_length=255, nullable=False)
+    description: str = Field(max_length=255, nullable=False)
+    quota: int = Field(nullable=False)
+    started_at: datetime = Field(nullable=False)
+    ended_at: datetime = Field(nullable=False)
     created_at: datetime = Field(
-        default_factory=datetime.now, sa_column_kwargs={"server_default": func.now()}
+        default_factory=datetime.now,
+        sa_column_kwargs={"server_default": func.now()},
+        nullable=False,
     )
+    update_at: datetime = Field(
+        default_factory=datetime.now,
+        sa_column_kwargs={"server_default": func.now()},
+        nullable=False,
+    )
+
+    registration: "Registration" = Relationship(back_populates="event")
+
+
+class Logs(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True, nullable=False)
+    account_id: int = Field(foreign_key="account_id")
+    created_at: datetime = Field(
+        default_factory=datetime.now,
+        sa_column_kwargs={"server_default": func.now()},
+        nullable=False,
+    )
+    action: str = Field(max_length=15, nullable=False)
+    ip_address: str = Field(max_length=25, nullable=False)
+    user_agent: str = Field(max_length=50, nullable=False)
+    entity: str = Field(max_length=50, nullable=False)
+    entity_id: int = Field(nullable=False)
+
+    account: "Account" = Relationship(back_populates="logs")
     updated_at: datetime = Field(
         default_factory=datetime.now,
         sa_column_kwargs={"onupdate": func.now(), "server_default": func.now()},
