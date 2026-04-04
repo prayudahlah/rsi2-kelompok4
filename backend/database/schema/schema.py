a60db153469eb370a294ecef98c153a9b575a584
Dien <akbardian64@gmail.com>
2026-04-04 10:53:37 +0700

    refactor: identasi registration

--- backend/database/schema/schema.py
+++ backend/database/schema/schema.py
@@ -2,20 +2,25 @@ from datetime import datetime, timezone
 from sqlmodel import SQLModel, Field, Relationship, func
 from typing import Optional
 
-class Event (SQLModel,table=True):
 
-    id : int = Field(default=None, primary_key=True, nullable=False)
-    name : str = Field(max_length=255, nullable=False)
-    description : str = Field(max_length=255, nullable=False)
-    quota : int = Field(nullable=False)
-    started_at : datetime =  Field(nullable=False)
-    ended_at : datetime = Field(nullable=False)
-    created_at : datetime = Field(default_factory= datetime.now, sa_column_kwargs={"server_default"
-        : func.now()}, nullable=False)
-    update_at: datetime =  Field(default_factory= datetime.now, sa_column_kwargs={"server_default"
-        : func.now()}, nullable=False)
+class Event(SQLModel, table=True):
+
+    id: int = Field(default=None, primary_key=True, nullable=False)
+    name: str = Field(max_length=255, nullable=False)
+    description: str = Field(max_length=255, nullable=False)
+    quota: int = Field(nullable=False)
+    started_at: datetime = Field(nullable=False)
+    ended_at: datetime = Field(nullable=False)
+    created_at: datetime = Field(
+        default_factory=datetime.now,
+        sa_column_kwargs={"server_default": func.now()},
+        nullable=False,
+    )
+    update_at: datetime = Field(
+        default_factory=datetime.now,
+        sa_column_kwargs={"server_default": func.now()},
+        nullable=False,
+    )
+
 
 registration: "Registration" = Relationship(back_populates="event")
-
-
-
