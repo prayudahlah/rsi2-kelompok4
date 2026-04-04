a2506ef67ba578e3b2bdef9d6d8ac3e9e1cba75f
bdyhauliaa <auliarahmabidayah26@gmail.com>
2026-04-04 11:25:44 +0700

    feat: add DDL for role table

--- backend/app/database/schema/schema.py
+++ backend/app/database/schema/schema.py
@@ -6,6 +6,6 @@ class Role(SQLModel, table=True):
     __tablename__ = "role"
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str = Field(max_length=50, unique=True, index=True)
+    name: str = Field(max_length=255, unique=True, index=True)
 
     accounts: List["Account"] = Relationship(back_populates="role")
