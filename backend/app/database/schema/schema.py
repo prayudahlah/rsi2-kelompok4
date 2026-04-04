f898e26f6d9f41a22d61e5984bbc00dfef50c3c5
Dien <akbardian64@gmail.com>
2026-04-04 12:15:58 +0700

    refactor: move database directory

---
+++ backend/app/database/schema/schema.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from sqlmodel import SQLModel, Field, Relationship, func
+from typing import Optional
+
+
+class Event(SQLModel, table=True):
+
+    id: Optional[int] = Field(default=None, primary_key=True, nullable=False)
+    name: str = Field(max_length=255, nullable=False)
+    description: str = Field(max_length=255, nullable=False)
+    quota: int = Field(nullable=False)
+    started_at: datetime = Field(nullable=False)
+    ended_at: datetime = Field(nullable=False)
+    created_at: datetime = Field(
+        default_factory=datetime.now,
+        sa_column_kwargs={"server_default": func.now()},
+        nullable=False,
+    )
+    update_at: datetime = Field(
+        default_factory=datetime.now,
+        sa_column_kwargs={"server_default": func.now()},
+        nullable=False,
+    )
+
+    registration: "Registration" = Relationship(back_populates="event")
+
+
+class Logs(SQLModel, table=True):
+
+    id: Optional[int] = Field(default=None, primary_key=True, nullable=False)
+    account_id: int = Field(foreign_key="account_id")
+    created_at: datetime = Field(
+        default_factory=datetime.now,
+        sa_column_kwargs={"server_default": func.now()},
+        nullable=False,
+    )
+    action: str = Field(max_length=15, nullable=False)
+    ip_address: str = Field(max_length=25, nullable=False)
+    user_agent: str = Field(max_length=50, nullable=False)
+    entity: str = Field(max_length=50, nullable=False)
+    entity_id: int = Field(nullable=False)
+
+    account: "Account" = Relationship(back_populates="logs")
