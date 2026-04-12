e3c9834d415e0d752ec615bda1e004a3cbc6b339
prayudahlah <yudafihan@gmail.com>
2026-04-12 10:26:18 +0700

    feat: dto for auth

--- backend/app/dto/auth.py
+++ backend/app/dto/auth.py
@@ -1,14 +1,39 @@
-from pydantic import BaseModel, EmailStr
+from sqlmodel import SQLModel
+from pydantic import EmailStr, field_validator
 
 
-class AuthRequest(BaseModel):
+class AccountRegister(SQLModel):
+    # Untuk table account
+    email: EmailStr
+    username: str
+    password: str
+
+    # Untuk table user
+    first_name: str
+    last_name: str
+    whatsapp: str
+
+    # Role enum
+    role_name: str = "user"  # Default role ke user
+
+    # Jika tidak valid juga default role ke user
+    @field_validator("role_name")
+    @classmethod
+    def validate_role(cls, v: str) -> str:
+        if not v or not v.strip():
+            return "user"
+        return v.lower()
+
+
+class AccountLogin(SQLModel):
     email: EmailStr
     password: str
 
 
-class TokenResponse(BaseModel):
+class TokenResponse(SQLModel):
     access_token: str
     token_type: str = "bearer"
+    account_id: int
 
     class Config:
         from_attribute = True
